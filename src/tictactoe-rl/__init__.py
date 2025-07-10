e16ef587dd65b1061db738e1293c4d20bb247fc6
Nate <nwinneg99@gmail.com>
2025-07-10 19:59:35 -0400

    Initial file structure commit

---
+++ src/tictactoe-rl/__init__.py
@@ -0,0 +1,9 @@
+"""
+tictactoe_rl
+============
+
+A Python package implementing reinforcement learning agents and utilities
+for the Tic Tac Toe game.
+"""
+
+from . import utils
---
+++ src/tictactoe-rl/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+tictactoe_rl
+
+Reinforcement Learning agents and utilities for Tic Tac Toe.
+"""
+
+from . import board
+from . import value_function_builder
